9831dfe69196459874c870c3c4febba562580bdf
Rayan-1 <rayanvictor088@gmail.com>
2023-09-18 23:21:00 -0300

    atualização

--- testjd/app.js
+++ testjd/app.js
@@ -1,11 +1,21 @@
 // app.js
 const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Docker World!\n');
+const app = express();
+
+app.use(bodyParser.json());
+
+const data = []; // Array para armazenar os dados cadastrados em memória
+
+app.post('/cadastro', (req, res) => {
+  const newData = req.body;
+  data.push(newData);
+  res.status(201).json({ message: 'Cadastro realizado com sucesso', data: newData });
 });
+
+const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 
